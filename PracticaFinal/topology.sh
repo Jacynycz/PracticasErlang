7e4adc1f5a14c5df47433880185f0a309c1b5dca
Viktor Jacynycz Garcia <viktor@jacynycz.es>
2018-02-09 16:16:03 +0100

    terminals

--- PracticaFinal/topology.sh
+++ PracticaFinal/topology.sh
@@ -3,9 +3,9 @@ then
     echo "Usage './topology.sh <terminal_command>'"
     exit 0
 fi
-$1 -e "erl -sname 1" 
-$1 -e "erl -sname 2"
-$1 -e "erl -sname 3"
-$1 -e "erl -sname 4"
-$1 -e "erl -sname 5"
-$1 -e "erl -sname 6"
+$1 -e "erl -sname 1" & 
+$1 -e "erl -sname 2" &
+$1 -e "erl -sname 3" &
+$1 -e "erl -sname 4" &
+$1 -e "erl -sname 5" &
+$1 -e "erl -sname 6" &
